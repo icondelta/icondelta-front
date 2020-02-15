6748f26a89cec6081b63a61442a91515b9669f40
ofnullable <ofnullable@gmail.com>
2020-02-15 21:16:33 +0900

    :construction: Change routing rules, add error page

--- src/pages/Trade.js
+++ src/pages/Trade.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Trade = () => {
-  return <div>Trade</div>;
+import { findToken } from '../common/utils';
+
+const Trade = ({ history, location, match }) => {
+  useEffect(() => {
+    const token = findToken(match.params.symbol);
+
+    if (!token) {
+      // replace unknown symbols to AC3
+      history.replace('/trade/AC3');
+    }
+  }, [match, location, history]);
+
+  return (
+    <div className="card" style={{ width: '100%' }}>
+      <div className="card__title">
+        <h1 style={{ color: '#1763b6', margin: 0 }}>Hello</h1>
+      </div>
+      <div className="card__body">
+        <p>icondelta</p>
+      </div>
+      <div className="card__footer">
+        <span>...</span>
+      </div>
+    </div>
+  );
 };
 
 export default Trade;
