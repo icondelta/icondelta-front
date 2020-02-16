bfa128cd2fb5b584e46f7985bf00262e16c41ab4
ofnullable <ofnullable@gmail.com>
2020-02-16 20:26:40 +0900

    :sparkles: Change current token to requested url

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
+import TokenContextProvider from '../contexts/TokenContext';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <TokenContextProvider>
       <Header />
       <main className="container">{children}</main>
       <Footer />
-    </>
+    </TokenContextProvider>
   );
 };
 
