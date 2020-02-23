1041837635be6040d3b4ff09d8c4bd86eb8b3da4
ofnullable <ofnullable@gmail.com>
2020-02-24 04:15:12 +0900

    :construction: Make trade-order component space

---
+++ src/components/trade/TradeList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TradeList = () => {
+  return <div className="card">dummy</div>;
+};
+
+export default TradeList;
--- src/pages/Trade.js
+++ src/pages/Trade.js
@@ -1,14 +1,49 @@
 import React from 'react';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 
 import Layout from '../layouts/Layout';
 import TokenList from '../components/token/TokenList';
 import TokenBalance from '../components/token/TokenBalance';
+import TradeList from '../components/trade/TradeList';
+import media from '../common/media';
+
+const wrapper = css`
+  display: flex;
+  width: calc(72% - 16px);
+  margin-top: 16px;
+
+  ${media.down('lg')} {
+    width: 100%;
+  }
+
+  div {
+    height: 80vh;
+    &:first-of-type {
+      margin-right: 8px;
+      flex: 1 1 auto;
+    }
+    &:nth-of-type(2) {
+      margin: 0 8px;
+      flex: 1.5 1 auto;
+    }
+    &:last-of-type {
+      margin-left: 8px;
+      flex: 1.3 1 auto;
+    }
+  }
+`;
 
 const Trade = props => {
   return (
     <Layout {...props}>
       <TokenList />
       <TokenBalance />
+      <div css={[wrapper]}>
+        <TradeList />
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
     </Layout>
   );
 };
