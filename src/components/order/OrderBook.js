2197b1cb0cd50fd99b2e1735304f0e3e3f26647f
ofnullable <ofnullable@gmail.com>
2020-03-01 21:06:52 +0900

    :art: Replace Order to OrderBook

--- src/components/order/OrderBook.js
+++ src/components/order/OrderBook.js
@@ -2,20 +2,46 @@ import React from 'react';
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 
+import { useTokenContext } from '../../contexts/TokenContext';
 import OrderBookItem from './OrderBookItem';
+import media from '../../common/media';
+import { tableHeader } from '../../styles/common';
 import { buyOrders, sellOrders } from '../../common/dummy';
 
-const orderBookWrapper = css`
-  padding: 0 4px 0 8px;
+const orderWrapper = css`
+  ${media.up('sm')} {
+    margin: 16px 16px 0;
+    flex: 1.4 1;
+  }
+  ${media.down('sm')} {
+    width: 100%;
+  }
 `;
 
-const OrderBook = ({ type }) => {
-  const orders = type === 'buy' ? buyOrders : sellOrders;
+const headerSize = css`
+  div {
+    flex: 1 0;
+  }
+`;
+
+const OrderBook = () => {
+  const { token } = useTokenContext();
+
   return (
-    <div css={[orderBookWrapper]}>
-      {orders.map(order => (
-        <OrderBookItem key={order.signature} data={order} />
-      ))}
+    <div className="card" css={[orderWrapper]}>
+      <div css={[tableHeader, headerSize]}>
+        <div>PRICE</div>
+        <div>{token.symbol}</div>
+        <div>ICX</div>
+      </div>
+      <div className="inner_scroll">
+        {buyOrders.map(order => (
+          <OrderBookItem key={order.signature} data={order} />
+        ))}
+        {sellOrders.map(order => (
+          <OrderBookItem key={order.signature} data={order} />
+        ))}
+      </div>
     </div>
   );
 };
