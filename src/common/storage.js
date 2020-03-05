12f6e836f70e3b291bed35e85a03c17afe7101e2
ofnullable <ofnullable@gmail.com>
2020-03-06 06:17:33 +0900

    :bug: Fix css, localstorage bugs

--- src/common/storage.js
+++ src/common/storage.js
@@ -1,4 +1,7 @@
-const get = key => localStorage.getItem(key);
+const get = key => {
+  const item = localStorage.getItem(key);
+  return item ? JSON.parse(item) : null;
+};
 
 const set = (key, item) => localStorage.setItem(key, JSON.stringify(item));
 
